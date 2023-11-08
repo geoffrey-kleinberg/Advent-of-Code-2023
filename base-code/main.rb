40188f2955c584956dbaea7827ded90bb4038048
Geoffrey Kleinberg <gkleinberg21@gmail.com>
2023-11-07 23:10:28 -0500

    fixed base code

--- base-code/main.rb
+++ base-code/main.rb
@@ -1,5 +1,15 @@
 file_name = 'sampleIn.txt'
-file_name = 'input.txt'
+# file_name = 'input.txt'
 
 data = File.read(file_name).split("\n").map { |i| i.strip }
 
+def part1(input)
+    return input
+end
+
+def part2(input)
+    return input
+end
+
+puts part1(data)
+# puts part2(data)
